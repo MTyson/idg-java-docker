60f2ad4772c7649601ac547ad9c591e222103865
matt tyson <matthewcarltyson@gmail.com>
2021-06-06 14:27:13 -0600

    rename dockerfile to Dockerfile

--- src/main/java/com/example/javadocker/DemoApplication.java
+++ src/main/java/com/example/javadocker/DemoApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
 @RequestMapping("/")
   public String home() {
-    return "Hello InfoWorld";
+    return "Hello InfoWorld!";
   }
 
 	public static void main(String[] args) {
